4f15277db8cf1f24582452e8d0285e20872f9d40
Qui Le <qnle@ucdavis.edu>
2019-12-13 15:18:41 -0800

    Instructions for implementation are slightly changed; Implemented the body of function sieve; Note: arrays in Rust cannot be dynamically created and allocated with memory space, and so I use vector. Also, we need usize constant in order to index into vector and thus I cast variable used as index into usize.

--- new_dawn/src/problem2.rs
+++ new_dawn/src/problem2.rs
@@ -5,17 +5,30 @@
 /// Find all prime numbers less than `n`
 /// For example, `sieve(7)` should return `[2, 3, 5]`
 pub fn sieve(n: u32) -> Vec<u32> {
+	
 	// Create a vector to store primes
-
+	let mut result_vec: Vec<u32> = Vec::new();
 	// Create a fixed array with size n storing 1 at first
 	// indexes from 2 to n-1 represent all numbers between 2 and n (we disregard indexes 0 and 1)
 	// ones and zeros inside array indicate if a number is prime or not
-	
+	let mut array = vec![1; n as usize];
 	// Iterate over a range of 2 through n
-		// we multiply a prime up to n
-		// we mark multiples of prime by placing 1 in array at that index 
-		// if we see a multiple we skip to next number in range
-
+	for number in 2..n {
+		if array[number as usize] == 1 { 
+			result_vec.push(number);
+			let mut count = 2;
+			let mut multiple = number * count; 
+			while multiple < n {
+				// we mark multiples of prime by placing 0 in array at that index 
+				array[multiple as usize] = 0;
+				// increment count
+				count += 1;
+				// we multiply a prime up to n
+				multiple = number * count;	
+			}
+		}
+		// otherwise, if we see a multiple we skip to next number in range
+	}
 	// Return the result vector
-
+	result_vec
 }
