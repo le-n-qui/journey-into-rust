dc2f7edbb1229eb1d86d94f3b0b82edab6ed6067
Qui Le <qnle@ucdavis.edu>
2019-12-25 23:09:25 -0800

    Change private function name into move_disc to avoid reserved word (move); Fix the parameters of move_disc (No longer use vectors, instead use u8 type to indicate source peg, target peg, and auxiliary peg); Implement the recursive algorithm for the hanoi problem

--- new_dawn/src/problem3.rs
+++ new_dawn/src/problem3.rs
@@ -5,15 +5,26 @@
 /// Solves for the sequence of moves required to move all 
 /// discs from peg 1 to peg 3, using peg 2 as an intermediary
 pub fn hanoi(num_discs: u32) -> Vec<(u8, u8)> {
-	// Make a call to private function move()
-	// with the given number of dics
-	
+	// Make a result vector
+	let mut result: Vec<(u8, u8)> = Vec::new();
+	// Make a call to private function move_disc()
+	// with the given number of discs
+	result.append(&mut move_disc(num_discs, 1, 3, 2));
+	// Return result
+	result
 }
 
-fn move(num_discs: u32, source: Vec<u32>, target: Vec<u32>, auxiliary: Vec<u32>) -> Vec<(u8, u8)> {
-	// If num_dics is bigger than zero
+fn move_disc(num_discs: u32, source: u8, target: u8, auxiliary: u8) -> Vec<(u8, u8)> {
+	// Create a vector to store the sequence of move
+	let mut move_vector: Vec<(u8, u8)> = Vec::new();
+	// If num_discs is bigger than zero
+	if num_discs > 0 {
 		// move num_discs - 1 from source to auxiliary
-		// Record the move sequence in a result vector
+		move_vector.append(&mut move_disc(num_discs - 1, source, auxiliary, target));
+		// Record the sequence of move for a particular disk
+		move_vector.push((source, target));
 		// move num_discs - 1 from auxiliary to target
-	
+		move_vector.append(&mut move_disc(num_discs - 1, auxiliary, target, source));
+	}
+	move_vector
 }
