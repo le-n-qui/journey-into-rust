5e81cf26950ca7d74dfeecee3c5abe6a86939eee
Qui Le <qnle@ucdavis.edu>
2019-12-25 20:50:17 -0800

    Include instructions for implementation of the private function move()

--- new_dawn/src/problem3.rs
+++ new_dawn/src/problem3.rs
@@ -5,11 +5,15 @@
 /// Solves for the sequence of moves required to move all 
 /// discs from peg 1 to peg 3, using peg 2 as an intermediary
 pub fn hanoi(num_discs: u32) -> Vec<(u8, u8)> {
-	// TODO
-	unimplemented!();	
+	// Make a call to private function move()
+	// with the given number of dics
+	
 }
 
 fn move(num_discs: u32, source: Vec<u32>, target: Vec<u32>, auxiliary: Vec<u32>) -> Vec<(u8, u8)> {
-	// TODO
-	unimplemented!()
+	// If num_dics is bigger than zero
+		// move num_discs - 1 from source to auxiliary
+		// Record the move sequence in a result vector
+		// move num_discs - 1 from auxiliary to target
+	
 }
