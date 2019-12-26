cf970d7439d96a5660e6e074ce34c69bf812f874
Qui Le <qnle@ucdavis.edu>
2019-12-25 20:01:20 -0800

    Create problem3.rs file; there is a private function to support the the client public function which is the hanoi() function

---
+++ new_dawn/src/problem3.rs
@@ -0,0 +1,15 @@
+// problem3.rs
+// Problem 3
+// Towers of Hanoi
+
+/// Solves for the sequence of moves required to move all 
+/// discs from peg 1 to peg 3, using peg 2 as an intermediary
+pub fn hanoi(num_discs: u32) -> Vec<(u8, u8)> {
+	// TODO
+	unimplemented!();	
+}
+
+fn move(num_discs: u32, source: Vec<u32>, target: Vec<u32>, auxiliary: Vec<u32>) -> Vec<(u8, u8)> {
+	// TODO
+	unimplemented!()
+}
