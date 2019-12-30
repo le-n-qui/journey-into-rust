79670268a6cf0f44c1749bded34f3742ffe68ab4
Qui Le <qnle@ucdavis.edu>
2019-12-30 14:51:38 -0800

    Create problem4.rs; Add 3 provided hashing functions; Add bloom function with no implementation yet

---
+++ new_dawn/src/problem4.rs
@@ -0,0 +1,45 @@
+// problem4.rs
+// Qui Le
+// 12/30/2019
+// Bloom Filter
+
+pub fn djb2(bytes: &[u8]) -> u64 {
+	let mut hash: u64 = 5381;
+	for b in bytes {
+		// hash * 33 + c
+		hash = (hash.wrapping_shr(5) + hash) + (*b as u64);
+	}
+	hash
+}
+
+pub fn fnv(bytes: &[u8]) -> u64 {
+	let mut hash = 0xcbf29ce484222325;
+	for b in bytes {
+		hash = hash ^ (*b as u64);
+		hash = hash.wrapping_mul(0x100000001b3);
+	}
+	hash
+}
+
+pub fn jenkins(bytes: &[u8]) -> u64 {
+	let mut hash = 0;
+	for b in bytes {
+		hash += *b as u64;
+		hash += hash.wrapping_shr(10);
+		hash ^= hash.wrapping_shl(6);
+	}
+	hash += hash.wrapping_shr(3);
+	hash ^= hash.wrapping_shl(11);
+	hash += hash.wrapping_shr(15);
+	hash
+}
+
+/// Simulates a bloom filter by accepting an array of three hash functions, a
+/// data vector, and another value to query. Returns `true` if `value` is
+/// "probably" in the data vector and `false` if it is definitely not in the 
+/// data vector.
+pub fn bloom(data: &Vec<&str>, hashes: [fn(&u[u8]) -> u64; 3], value: &str) -> bool {
+	// TODO
+	unimplemented!();	
+
+}
