982b019bdab115855f3eda38dd82237d8393a230
Qui Le <qnle@ucdavis.edu>
2020-01-31 16:20:17 -0800

    Add #[derive(Debug)] before each of the 3 type definitions for the purpose of debug-printting

--- giant_trees/src/first.rs
+++ giant_trees/src/first.rs
@@ -3,17 +3,20 @@
 // Qui Le
 
 // public struct for Binary Search Tree
+#[derive(Debug)]
 pub struct BST {
 	root: Link,
 }
 
 // private Link enum
+#[derive(Debug)]
 enum Link {
 	Empty,
 	More(Box<Node>),
 }
 
 // private Node struct
+#[derive(Debug)]
 struct Node {
 	// Data inside node
 	data: i32,
